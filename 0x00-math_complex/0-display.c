fd8658c92fdcc95eb941cc1dceeebe96086ea7d0
root <jesus4388>
2022-04-27 19:08:28 -0700

    complex

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -7,12 +7,18 @@ void display_complex_number(complex c)
 	{
 		if (c.im > 0 && c.im != 1)
 			printf("%g + %gi\n", c.re, c.im);
-		else if (c.im < 0 && c.im != -1)
+		if (c.im < 0 && c.im != -1)
+		{
+			c.im = c.im * -1;
 			printf("%g - %gi\n", c.re, c.im);
+		}
 		else if (c.im == 1)
-			printf("%g\n + i", c.re);
+			printf("%g + i\n", c.re);
 		else if (c.im == -1)
-			printf("%g\n - i", c.re);
+		{
+			c.im = c.im * -1;
+			printf("%g - i\n", c.re);
+		}
 		else
 			printf("%g\n", c.re);
 	}
--- 0x00-math_complex/0-main.c
+++ 0x00-math_complex/0-main.c
@@ -12,7 +12,10 @@ int main(void)
     complex c1;
 
     c1.re = 1;
-    c1.im = 2;
+    c1.im = -2;
+    display_complex_number(c1);
+    c1.re = 1;
+    c1.im = 1;
     display_complex_number(c1);
     c1.re = 1;
     c1.im = 0;
